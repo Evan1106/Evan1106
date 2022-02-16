8bee6b713fa27bd5d4e83d350dd2e29e3b379047
Evan1106 <93518713+Evan1106@users.noreply.github.com>
2022-02-16 10:45:08 +0800

    ES6 complete

--- github/exercise_getRepo.js
+++ github/exercise_getRepo.js
@@ -12,11 +12,12 @@ import Timer from './timer/index.js';
 // 常數
 const owner = 'Evan1106';
 const repo = 'repoAPITry';
+const info = [{owner: owner, repo: repo}];
 
 // 全域變數/函式
 const getRepoAPI = async (owner, repo) => {
   // TODO: 把 key 等重要資訊移到 .env file
-  const clientIdAndSecret = 'Evan1106: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';
+  const clientIdAndSecret = 'Evan1106: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';
   const base64 = (text) => Buffer
     .from(text)
     .toString('base64');
@@ -52,12 +53,12 @@ const getUsersES6 = async () => {
   const timer = new Timer();
 
   // Why [...users]?
-  const promises = async (owner, repo) => {
-    const userDetails = await getRepoAPI(owner, repo);
+  const promises = [...info].map(async (info) => {
+    const userDetails = await getRepoAPI(info.owner, info.repo);
 
     // Why not await?
     return userDetails.json();
-  };
+  });
 
   const results = await Promise.all(promises);
 
@@ -68,7 +69,7 @@ const getUsersES6 = async () => {
 
 (async () => {
   const result_1 = await getUsers();
-  // const result_2 = await getUsersES6();
+  const result_2 = await getUsersES6();
 
   // 在我的主機上列印出來的：
   // Why 兩者數值差這麼多？
